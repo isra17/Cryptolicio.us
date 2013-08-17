c4511bb5f7e266fc91e02a03b2d5625c8ff3bb37
Israel Halle <isra017@gmail.com>
2013-08-17 04:38:54 -0400

    Refactor Contacts key page

--- src/options/options.js
+++ src/options/options.js
@@ -58,7 +58,13 @@
       for(var k=0;k<keys.length;k++){
           var key = keys[k];
           var user = gCryptUtil.parseUser(key.obj.userIds[0].text);
-          $('#publicKeyTable>tbody').append('<tr><td class="removeLink" id="'+k+'"><a href="#">remove</a></td><td>'+user.userName+'</td><td>'+user.userEmail+'</td><td>'+util.hexstrdump(key.keyId)+'</td><td><a href="#public'+k+'" data-toggle="modal">show key</a><div class="modal" id="public'+k+'"><a href="#" class="close" data-dismiss="modal">Close</a><br/ >'+key.armored.replace(/\n/g,'<br/ >')+'</div></td></tr>');
+          $('#publicKeyTable>tbody').append(
+              '<tr><td>'+user.userName+'</td>'+
+              '<td>'+user.userEmail+'</td>'+
+              '<td>'+util.hexstrdump(key.keyId)+'</td>'+
+              '<td><a href="#public'+k+'" data-toggle="modal">Show key</a><div class="modal" id="public'+k+'">'+
+                  '<a href="#" class="close" data-dismiss="modal">Close</a><br/ ><textarea>'+key.armored+'</textarea></div></td>'+
+                  '<td class="removeLink" id="'+k+'"><a href="#">Remove</a></td></tr>');
           $('#public'+k).hide();
           $('#public'+k).modal({backdrop: true, show: false});
       }
@@ -78,9 +84,9 @@
           $('#privateKeyTable>tbody').append(
               '<tr><td>'+user.userName+'</td>'+
               '<td>'+user.userEmail+'</td>'+
-              '<td><a href="#private'+k+'" data-toggle="modal">show key</a><div class="modal" id="private'+k+'">'+ 
+              '<td><a href="#private'+k+'" data-toggle="modal">Show key</a><div class="modal" id="private'+k+'">'+ 
                   '<a class="close" data-dismiss="modal">Close</a><br/ ><textarea>'+key.armored+'</textarea></div></td>'+
-              '<td class="removeLink" id="'+k+'"><a href="#">remove</a></td></tr>');
+              '<td class="removeLink" id="'+k+'"><a href="#">Remove</a></td></tr>');
           $('#private'+k).hide();
           $('#private'+k).modal({backdrop: true, show: false});
       }
