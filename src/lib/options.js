9bbe31df78509374b0786b8debc8b259d649da3f
Israel Hallé <isra017@gmail.com>
2013-08-17 02:10:07 -0400

    Upload generated key on key server

--- src/lib/options.js
+++ src/lib/options.js
@@ -1,11 +1,17 @@
 (function(context){
-    var Crypt{us:Options:{}};
+    var Crypt = {us: {Options:{}}};
 
-    Crypt.us.Options.generateKeyPair(data){
+    Crypt.us.Options.generateKeyPair = function(data){
         var form = $('#generateKeyPairForm');
         var keyPair = openpgp.generate_key_pair(1,data.bits, data.name + ' <' + data.email + '>', data.password);
         openpgp.keyring.importPrivateKey(keyPair.privateKeyArmored, data.password);
         openpgp.keyring.importPublicKey(keyPair.publicKeyArmored);
         openpgp.keyring.store();
-   }
+
+        return keyPair;
+    }
+
+    openpgp.init();
+
+    context.Crypt = Crypt;
 })(window);
--- src/setup/setup.js
+++ src/setup/setup.js
@@ -1,5 +1,22 @@
 (function(){
-    $('#key-form').submit(function(){
+    $(function(){
+        $('#key-form').submit(function(e){
+            var form = $(this);
+            var data = {
+                bits: 2048,
+                name: form.find('input[name=name]').val(),
+                email: form.find('input[name=email]').val(),
+                password: form.find('input[name=password]').val()
+            };
 
-    });
+            var keyPair = Crypt.us.Options.generateKeyPair(data);
+            if(form.find('input[name=mit]').val() === "on") {
+                $.post('http://pgp.mit.edu:11371/pks/add', {
+                    keytext: keyPair.publicKeyArmored
+                });
+            }
+
+            return e.preventDefault();
+        });
+    })
 })();
